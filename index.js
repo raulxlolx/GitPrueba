7104aba5dc3064cf0b9b5afe3106833a161cbe85
raulxlolx <91206628+raulxlolx@users.noreply.github.com>
2023-10-05 12:40:21 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express =require('express')
+const path = require('path')
+const app =express()
+
+app.get('/', function(req,res){
+  res.sendFile(path.join(__dirname, 'index.html'));
+})
+app.listen(3000);
